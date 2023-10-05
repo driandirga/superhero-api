bc4e15a0bb9c10d5bcd0d3b75e378bfaec9e901e
Drian Dirga <driandirga@gmail.com>
2023-10-05 09:13:00 +0700

    Create project SuperHeroAPI

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var heros = new List<SuperHero>
+            {
+                new SuperHero {
+                    Id = 1,
+                    Name = "Spederman",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Power = "Spider Webs",
+                    IsFly = true,
+                }
+            };
+        }
+    }
+}
---
+++ SuperHeroAPI/SuperHero.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperHeroAPI
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Power { get; set; } = string.Empty;
+        public bool IsFly { get; set; } = true;
+    }
+}
