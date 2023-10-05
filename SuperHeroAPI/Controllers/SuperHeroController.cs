3ac31ab232a32417b884399fc7415bceb2ebea49
Drian Dirga <driandirga@gmail.com>
2023-10-05 13:24:57 +0700

    Connection to SQL Server with Entity Framework Core

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -26,21 +26,28 @@ namespace SuperHeroAPI.Controllers
                     IsFly = true,
                 }
             };
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
 
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> Get()
         {
+            var heroes = await _context.SuperHeroes.ToListAsync();
             if (heroes.Count == 0)
-                return BadRequest("Heroes doesn't exist");
+                return BadRequest("Heroes doesn't exist.");
             return Ok(heroes);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<SuperHero>> Get(int id)
         {
-            var hero = heroes.Find(h => h.Id == id);
-            if (hero == null)
-                return BadRequest($"Hero with id {id} not found");
+            var hero = await _context.SuperHeroes.FindAsync(id);
+            if (hero == null) 
+                return BadRequest($"Hero with id {id} not found.");
 
             return Ok(hero);
         }
@@ -48,16 +55,21 @@ namespace SuperHeroAPI.Controllers
         [HttpPost]
         public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
         {
-            heroes.Add(hero);
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+
+            var heroes = await _context.SuperHeroes.ToListAsync();
+            if (heroes.Count == 0)
+                return BadRequest("Heroes doesn't exist.");
             return Ok(heroes);
         }
 
         [HttpPut]
         public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
         {
-            var hero = heroes.Find(h => h.Id == request.Id);
+            var hero = await _context.SuperHeroes.FindAsync(request.Id);
             if (hero == null)
-                return BadRequest($"Hero with id {request.Id} not found");
+                return BadRequest($"Hero with id {request.Id} not found.");
 
             hero.Name = request.Name;
             hero.FirstName = request.FirstName;
@@ -65,17 +77,25 @@ namespace SuperHeroAPI.Controllers
             hero.Power = request.Power;
             hero.IsFly = request.IsFly;
 
+            var heroes = await _context.SuperHeroes.ToListAsync();
+            if (heroes.Count == 0)
+                return BadRequest("Heroes doesn't exist.");
             return Ok(heroes);
         }
 
         [HttpDelete]
         public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
         {
-            var hero = heroes.Find(h => h.Id == id);
+            var hero = await _context.SuperHeroes.FindAsync(id);
             if (hero == null)
-                return BadRequest($"Hero with id {id} not found");
+                return BadRequest($"Hero with id {id} not found.");
 
-            heroes.Remove(hero);
+            _context.SuperHeroes.Remove(hero);
+            await _context.SaveChangesAsync();
+
+            var heroes = await _context.SuperHeroes.ToListAsync();
+            if (heroes.Count == 0)
+                return BadRequest("Heroes doesn't exist.");
             return Ok(heroes);
         }
     }
---
+++ SuperHeroAPI/Migrations/20231005045115_CreateInitial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SuperHeroAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SuperHeroes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Power = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsFly = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperHeroes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SuperHeroes");
+        }
+    }
+}
